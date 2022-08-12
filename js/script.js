45405000233a0955a74f1d61e947fa0f93be14e9
sujaypaul <sujaypaul031200@gmail.com>
2022-08-12 15:24:17 +0530

    Added form validation, invalid-feedback appers below the input area, bug : still adds the data to the row even when input area is invalid

--- js/script.js
+++ js/script.js
@@ -2,14 +2,98 @@ const pageAnimation = () => {
   document.querySelector('body').style.opacity = 1;
 };
 
+// (function () {
+//   'use strict';
+//   window.addEventListener('load', function () {
+//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//       var forms = document.getElementsByClassName('needs-validation');
+//       // Loop over them and prevent submission
+//       var validation = Array.prototype.filter.call(forms, function (form) {
+//           form.addEventListener('submit', function (event) {
+
+//               var failed = false;
+
+//               if ($("[name='select']:checked").length == 0) {
+//                   $("[name='select']").attr('required', true);
+//                   failed = true;
+//               }
+//               else {
+//                   $("[name='select']").attr('required', false);
+//               }
+
+//               if (form.checkValidity() === false) {
+//                   failed = true;
+//               }
+
+//               if (failed == true) {
+//                   event.preventDefault();
+//                   event.stopPropagation();
+//               }
+
+//               form.classList.add('was-validated');
+//           }, false);
+//       });
+//   }, false);
+// })();
+
+(function () {
+  'use strict'
+  const forms = document.querySelectorAll('.requires-validation')
+  Array.from(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+
+    window.addEventListener('load', function () {
+           // Fetch all the forms we want to apply custom Bootstrap validation styles to
+           var forms = document.getElementsByClassName('needs-validation');
+           // Loop over them and prevent submission
+           var validation = Array.prototype.filter.call(forms, function (form) {
+               form.addEventListener('submit', function (event) {
+      
+                   var failed = false;
+      
+                   if ($("[name='select']:checked").length == 0) {
+                       $("[name='select']").attr('required', true);
+                       failed = true;
+                   }
+                   else {
+                       $("[name='select']").attr('required', false);
+                   }
+      
+                   if (form.checkValidity() === false) {
+                       failed = true;
+                   }
+      
+                   if (failed == true) {
+                       event.preventDefault();
+                       event.stopPropagation();
+                   }
+      
+                   form.classList.add('was-validated');
+               }, false);
+           });
+       }, false);
+
+
+  })()
 
 let flag = false;
 
 const onSubmit = () => {
+
   let gender;
   let data = [];
   let skills = [];
-
+  
   const name = document.getElementById('inputName').value;
   const email = document.getElementById('inputEmail').value;
   const website = document.getElementById('inputWebsite').value;
