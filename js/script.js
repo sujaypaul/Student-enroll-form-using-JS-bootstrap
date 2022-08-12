49777dc0cd926679719d9e039375d67e6c017b71
sujaypaul <sujaypaul031200@gmail.com>
2022-08-12 15:46:39 +0530

    minor adjustments and cleanup

--- js/script.js
+++ js/script.js
@@ -2,39 +2,7 @@ const pageAnimation = () => {
   document.querySelector('body').style.opacity = 1;
 };
 
-// (function () {
-//   'use strict';
-//   window.addEventListener('load', function () {
-//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//       var forms = document.getElementsByClassName('needs-validation');
-//       // Loop over them and prevent submission
-//       var validation = Array.prototype.filter.call(forms, function (form) {
-//           form.addEventListener('submit', function (event) {
 
-//               var failed = false;
-
-//               if ($("[name='select']:checked").length == 0) {
-//                   $("[name='select']").attr('required', true);
-//                   failed = true;
-//               }
-//               else {
-//                   $("[name='select']").attr('required', false);
-//               }
-
-//               if (form.checkValidity() === false) {
-//                   failed = true;
-//               }
-
-//               if (failed == true) {
-//                   event.preventDefault();
-//                   event.stopPropagation();
-//               }
-
-//               form.classList.add('was-validated');
-//           }, false);
-//       });
-//   }, false);
-// })();
 
 (function () {
   'use strict'
